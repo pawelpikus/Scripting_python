4c18a66688eaa218237a11e1405b4a788ac6cb5e
Paweł Pikus <pawelpikus7@gmail.com>
2020-05-24 17:40:18 +0200

    SMTP - email_sender.py with html Template

---
+++ email_sender.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Paweł Pikus'
+email['to'] = 'dev.tester1982@gmail.com'
+email['subject'] = 'You won 10000 rubles!'
+
+email.set_content(html.substitute({'name': 'Paweł'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('dev.tester1982@gmail.com', 'Devtest12!@')
+    smtp.send_message(email)
+    print('Done!')
